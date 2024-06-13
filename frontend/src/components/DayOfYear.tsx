a5df106101163a758603ca4a9efa9bded0d06a14
krasi <krasimir.xristov@gmail.com>
2024-06-13 19:01:48 +0200

    add new proparty in the languages Json files

--- frontend/src/components/DayOfYear.tsx
+++ frontend/src/components/DayOfYear.tsx
@@ -1,15 +1,22 @@
 // src/components/DayOfYear.tsx
 
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 const DayOfYear: React.FC = () => {
+  const { t } = useTranslation();
+
   const now = new Date();
   const start = new Date(now.getFullYear(), 0, 0);
   const diff = now.getTime() - start.getTime();
   const oneDay = 1000 * 60 * 60 * 24;
   const day = Math.floor(diff / oneDay);
 
-  return <div>Day of the year: {day}</div>;
+  return (
+    <div>
+      {t('dayOfYear')}: {day}
+    </div>
+  );
 };
 
 export default DayOfYear;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import DayOfYear from '../components/DayOfYear';
 
 const Home: React.FC = () => {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen text-center px-4 pt-12 md:pt-0'>
+    <div className='flex flex-col items-center justify-center min-h-screen text-center px-4 pt-12 md:pt-0 '>
       <h1 className='text-6xl md:text-8xl mb-8'>Home Page</h1>
       <DayOfYear />
     </div>

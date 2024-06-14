f007587012002ca91292695ff053252983eb573f
krasi <krasimir.xristov@gmail.com>
2024-06-14 09:11:32 +0200

    Make better UI in Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,8 +6,10 @@ import { useTranslation } from 'react-i18next';
 const Home: React.FC = () => {
   const { t } = useTranslation();
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen text-center px-4 pt-12 md:pt-0 '>
-      <h1 className='text-4xl md:text-4xl lg:text-6xl mb-8'>{t('welcome')}</h1>
+    <div className='flex flex-col items-center justify-center min-h-screen text-center px-4 pt-12 md:pt-0 bg-slate-200'>
+      <h1 className='text-3xl md:text-4xl lg:text-6xl mb-8 font-bold'>
+        {t('welcome')}
+      </h1>
       <DayOfYear />
       <CurrentDate />
     </div>

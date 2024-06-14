31849e487a5ef40ecd89017b5ea45efb1cb16dab
krasi <krasimir.xristov@gmail.com>
2024-06-14 09:02:28 +0200

    create a responsive welcome tag in the home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import DayOfYear from '../components/DayOfYear';
 import CurrentDate from '../components/CurrentDate';
+import { useTranslation } from 'react-i18next';
 
 const Home: React.FC = () => {
+  const { t } = useTranslation();
   return (
     <div className='flex flex-col items-center justify-center min-h-screen text-center px-4 pt-12 md:pt-0 '>
-      <h1 className='text-6xl md:text-8xl mb-8'>Home Page</h1>
+      <h1 className='text-4xl md:text-4xl lg:text-6xl mb-8'>{t('welcome')}</h1>
       <DayOfYear />
       <CurrentDate />
     </div>

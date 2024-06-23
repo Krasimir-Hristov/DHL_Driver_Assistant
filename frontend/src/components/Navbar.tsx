0639fbb263625f6e0a47c2b2cf8ae2ffc04756aa
krasi <krasimir.xristov@gmail.com>
2024-06-23 18:31:12 +0200

    change the ISO code of the contry with the national flag, in the Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,12 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FlagIcon } from 'react-flag-kit';
 import { useTranslation } from 'react-i18next';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
+interface Language {
+  code: string;
+  lang: string;
+}
+
+const languages: Language[] = [
+  { code: 'GB', lang: 'en' },
+  { code: 'BG', lang: 'bg' },
+  { code: 'RO', lang: 'ro' },
+  { code: 'DE', lang: 'de' },
+  { code: 'AL', lang: 'al' },
+  { code: 'GR', lang: 'gr' },
+  { code: 'IT', lang: 'it' },
+  { code: 'TR', lang: 'tr' },
+  { code: 'RS', lang: 'ru' },
+  { code: 'HU', lang: 'hu' },
+];
+
 const Navbar: React.FC = () => {
-  const { t, i18n } = useTranslation();
+  const { i18n } = useTranslation();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [currentLanguage, setCurrentLanguage] = useState<Language>({
+    code: 'GB',
+    lang: 'en',
+  });
+
+  useEffect(() => {
+    const currentLang = languages.find((lang) => lang.lang === i18n.language);
+    if (currentLang) {
+      setCurrentLanguage(currentLang);
+    }
+  }, [i18n.language]);
 
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
@@ -22,8 +51,8 @@ const Navbar: React.FC = () => {
       <div className='container mx-auto flex justify-between items-center'>
         <Link to='/' className='text-red-700 text-2xl font-bold'>
           <div className='flex gap-2 items-center'>
-            <p>DHL Driver Assistant</p>
-            <p className='text-black font-extrabold'>{t('indicator')}</p>
+            <p className='text-3xl'>DHL Driver Assistant</p>
+            <FlagIcon code={currentLanguage.code as any} size={24} />
           </div>
         </Link>
         <div className='md:hidden'>
@@ -41,18 +70,7 @@ const Navbar: React.FC = () => {
           } md:flex md:items-center`}
         >
           <div className='flex flex-wrap justify-center md:justify-start'>
-            {[
-              { code: 'GB', lang: 'en' },
-              { code: 'BG', lang: 'bg' },
-              { code: 'RO', lang: 'ro' },
-              { code: 'DE', lang: 'de' },
-              { code: 'AL', lang: 'al' },
-              { code: 'GR', lang: 'gr' },
-              { code: 'IT', lang: 'it' },
-              { code: 'TR', lang: 'tr' },
-              { code: 'RS', lang: 'ru' },
-              { code: 'HU', lang: 'hu' },
-            ].map(({ code, lang }) => (
+            {languages.map(({ code, lang }) => (
               <button
                 key={lang}
                 onClick={() => changeLanguage(lang)}
@@ -62,20 +80,6 @@ const Navbar: React.FC = () => {
               </button>
             ))}
           </div>
-          {/* <div className='flex flex-col md:flex-row justify-center md:justify-start mt-4 md:mt-0'>
-            <Link
-              to='/login'
-              className='text-black hover:text-white mx-2 font-bold'
-            >
-              {t('login')}
-            </Link>
-            <Link
-              to='/register'
-              className='text-black hover:text-white mx-2 font-bold'
-            >
-              {t('register')}
-            </Link>
-          </div> */}
         </div>
       </div>
     </nav>

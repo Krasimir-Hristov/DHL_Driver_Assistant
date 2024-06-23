5a2135d4a9b65325ad2d7f9ef9ee55d85825fc3b
krasi <krasimir.xristov@gmail.com>
2024-06-23 18:40:44 +0200

    add the language indicator in the footer near the logo

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const Footer: React.FC = () => {
+  const { t } = useTranslation();
   return (
     <footer className='bg-yellow-500 p-4'>
       <div className='container mx-auto flex flex-col md:flex-row justify-between items-center text-center md:text-left'>
         <Link to='/' className='text-red-700 text-2xl font-bold'>
           <div className='flex gap-2 items-center'>
             <p>DHL Driver Assistant</p>
-            {/* <p className='text-black font-extrabold'>{t('indicator')}</p> */}
+            <p className='text-black font-extrabold'>{t('indicator')}</p>
           </div>
         </Link>
         <p className='text-black font-bold text-lg mt-2 md:mt-0'>
